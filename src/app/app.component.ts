350bde15f5f3ac94c5a3c96f58d8d019f90eb4d0
dengdeng123ni <2648736010@qq.com>
2023-04-04 18:43:41 +0800

    更新

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,31 @@ export class AppComponent {
         this.ContextMenuController.close();
       }
     })
+    this.getPlatform();
 
+  }
+  getPlatform() {
+    const ua = navigator.userAgent.toLowerCase();
+    const platform: any = {};
+    const MAP_EXP = {
+      Mac: /(mac os x)\s+([\w_]+)/,
+      Windows: /(windows nt)\s+([\w.]+)/,
+      Ios: /(i(?:pad|phone|pod))(?:.*)cpu(?: i(?:pad|phone|pod))? os (\d+(?:[\.|_]\d+){1,})/,
+      Android: /(android)\s+([\d.]+)/,
+      Ipad: /(ipad).*os\s([\d_]+)/,
+      Iphone: /(iphone\sos)\s([\d_]+)/,
+    };
+    for (let key in MAP_EXP) {
+      const uaMatch = ua.match(MAP_EXP[key]);
+      if (!!uaMatch) {
+        if(!document.body.getAttribute("operate")){
+          if("Windows"==key) {
+            document.body.setAttribute("system", "windows")
+          }
+        }
+
+      }
+    }
   }
   async separateImage(src) {
     const req = await fetch(src);
