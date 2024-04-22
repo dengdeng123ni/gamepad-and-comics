e54e9f663380e7e64a9538c9978ecf7efdfcaec7
等等123你 <5358997+dengdeng123ni@user.noreply.gitee.com>
2024-04-22 20:34:42 +0800

    更新

--- src/app/pages/list/list.module.ts
+++ src/app/pages/list/list.module.ts
@@ -6,14 +6,8 @@ import { IndexComponent } from './pages/index/index.component';
 import { MaterialModule } from 'src/app/library/material.module';
 import { ComicsListComponent } from './components/comics-list/comics-list.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { QueryComponent } from './components/query/query.component';
-import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { EditToolbarComponent } from './components/edit-toolbar/edit-toolbar.component';
-import { ComicsQueryTypeComponent } from './components/comics-query-type/comics-query-type.component';
 import { ComicsSelectTypeComponent } from './components/comics-select-type/comics-select-type.component';
 import { ComicsUpdateComponent } from './components/comics-update/comics-update.component';
-import { MenuTopToolbarComponent } from './components/menu-top-toolbar/menu-top-toolbar.component';
 import { TemporaryFileComponent } from './components/temporary-file/temporary-file.component';
 import { HistoryComponent } from './components/history/history.component';
 import { DownloadOptionComponent } from './components/download-option/download-option.component';
@@ -27,14 +21,8 @@ import { ComicsQueryComponent } from './components/comics-query/comics-query.com
     IndexComponent,
     ComicsListComponent,
     MenuComponent,
-    QueryComponent,
-    BookmarksComponent,
-    ToolbarComponent,
-    EditToolbarComponent,
-    ComicsQueryTypeComponent,
     ComicsSelectTypeComponent,
     ComicsUpdateComponent,
-    MenuTopToolbarComponent,
     TemporaryFileComponent,
     HistoryComponent,
     DownloadOptionComponent,
