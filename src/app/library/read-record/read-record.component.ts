b1a8d56f01dc916b5210aa448b4e24650bcf4e16
等等123你 <5358997+dengdeng123ni@user.noreply.gitee.com>
2024-08-14 02:30:10 +0800

    更新

--- src/app/library/read-record/read-record.component.ts
+++ src/app/library/read-record/read-record.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
 import { firstValueFrom } from 'rxjs';
+import { ReadRecordService } from './read-record.service';
+import { Router } from '@angular/router';
+import { DbControllerService } from '../public-api';
 
 @Component({
   selector: 'app-read-record',
@@ -9,37 +12,36 @@ import { firstValueFrom } from 'rxjs';
 })
 export class ReadRecordComponent {
   list = [];
-  constructor(public webDb: NgxIndexedDBService,) {
+  constructor(public webDb: NgxIndexedDBService, public ReadRecord: ReadRecordService, public router: Router,public DbController:DbControllerService) {
     this.init();
   }
 
   async init() {
     const read_record: any = await firstValueFrom(this.webDb.getAll('read_record'));
     const details: any = await firstValueFrom(this.webDb.getAll('details'));
-    let list = this.uniqueFunc(read_record, 'comics_id');
+    let list = read_record.reverse()
+
     list.forEach(x => {
       x.date = this.formaData(x.id)
       x.day = x.date.substring(0, 10)
       x.data = (details.find(c => c.id == x.comics_id))?.data
     })
-    list=list.filter(x=>!!x.data)
-    list.forEach(x=>{
-      x.data.cover=`http://localhost:7700/${x.origin}/comics/${x.data.id}`;
+
+    list = list.filter(x => !!x.data)
+    list.forEach(x => {
+      x.data.cover = `http://localhost:7700/${x.origin}/comics/${x.data.id}`;
     })
-    const days = [...new Set(list.map(x => x.day))];
+    const days = [...new Set(list.map(x => x.day))].reverse();
     let arr = [];
     days.forEach(x => {
       arr.unshift(JSON.parse(JSON.stringify({
         day: x,
-        list: list.filter(c => c.day == x)
+        list: this.uniqueFunc(list.filter(c => c.day == x), 'comics_id').sort((a,b)=>b.id-a.id)
       })))
     })
-    this.list=arr;
     console.log(arr);
 
-
-
-
+    this.list = arr;
   }
   uniqueFunc(arr, uniId) {
     const res = new Map();
@@ -60,5 +62,16 @@ export class ReadRecordComponent {
     return `${pad(year, 4)}-${pad(month)}-${pad(day)} ${pad(hour)}:${pad(minute)}:${pad(second)}`
   }
 
+  close() {
+    this.ReadRecord.close();
+  }
 
+  async routerReader(origin, comics_id) {
+    const _res: any = await Promise.all([this.DbController.getDetail(comics_id), await firstValueFrom(this.webDb.getByID("last_read_comics", comics_id.toString()))])
+    if (_res[1]) {
+      this.router.navigate(['/comics', origin, comics_id, _res[1].chapter_id])
+    } else {
+      this.router.navigate(['/comics', origin, comics_id, _res[0].chapters[0].id])
+    }
+  }
 }
