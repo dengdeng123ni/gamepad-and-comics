0123ecf2f24f5e9ec29c08a8960f76bfeb554ded
等等123你 <5358997+dengdeng123ni@user.noreply.gitee.com>
2025-01-10 15:59:52 +0800

    更新

---
+++ android/app/src/main/java/gamepad/and/comics/MainActivity.java
@@ -0,0 +1,5 @@
+package gamepad.and.comics;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'gamepad.and.comics',
+  appName: 'gamepad-and-comics',
+  webDir: 'dist'
+};
+
+export default config;
