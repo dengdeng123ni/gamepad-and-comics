3713b0152d55ab40346d9ccd027c3582aa08fb63
等等123你 <5358997+dengdeng123ni@user.noreply.gitee.com>
2025-01-11 09:48:32 +0800

    更新

--- src/app/pages/list/components/page-start/page-start.component.ts
+++ src/app/pages/list/components/page-start/page-start.component.ts
@@ -1,26 +1,70 @@
 import { Component } from '@angular/core';
 
+import CryptoJS from 'crypto-js'
 @Component({
   selector: 'app-page-start',
   templateUrl: './page-start.component.html',
   styleUrl: './page-start.component.scss'
 })
 export class PageStartComponent {
-  is_google=false;
-  is_baidu=false;
-  async on(){
-    try {
-      const res=  await window._gh_fetch("https://www.google.com")
-      this.is_google=true;
-    } catch (error) {
-      this.is_google=false;
-    }
-    try {
-      await window._gh_fetch("https://www.baidu.com/")
-      this.is_baidu=true;
-    } catch (error) {
-      this.is_baidu=false;
-    }
+  is_google = false;
+  is_baidu = false;
+  is_hentai = false;
+  id = ""
+  list = [];
+  list2 = [];
+  arr = [];
+  async on() {
+
+    // setTimeout(async () => {
+    //   try {
+    //     await window._gh_fetch("https://www.google.com")
+    //     this.is_google = true;
+    //   } catch (error) {
+
+    //     this.is_google = false;
+    //     this.arr.push(error)
+    //   }
+    // })
+    // setTimeout(async () => {
+    //   try {
+    //     await window._gh_fetch("https://www.baidu.com/")
+    //     this.is_baidu = true;
+    //   } catch (error) {
+    //     this.is_baidu = false;
+    //     this.arr.push(error)
+    //   }
+    // })
+
+    // setTimeout(async () => {
+    //   try {
+    //     const res = await window._gh_fetch("https://e-hentai.org/")
+    //     this.is_hentai = true;
+    //   } catch (error) {
+    //     this.is_hentai = false;
+    //     this.arr.push(error)
+    //   }
+    // })
+
+
+    // setTimeout(async () => {
+    //   try {
+    //     const id = CryptoJS.MD5(JSON.stringify({ data: "obn", source: "option.source" })).toString().toLowerCase();
+    //     this.id=id;
+    //   } catch (error) {
+    //     this.arr.push(error)
+    //   }
+    // })
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/assets/loadable-js/ehentai_data_source.js
+++ src/assets/loadable-js/ehentai_data_source.js
@@ -1086,7 +1086,8 @@ window._gh_comics_register({
           "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
           "sec-ch-ua": "\"Microsoft Edge\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""
         },
-        mode: "cors"
+        mode: "cors",
+        responseType: 'blob'
       })
       const blob = await res.blob();
       return blob
@@ -1118,7 +1119,8 @@ window._gh_comics_register({
             "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
             "sec-ch-ua": "\"Microsoft Edge\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""
           },
-          mode: "cors"
+          mode: "cors",
+          responseType: 'blob'
         });
         const blob = await res.blob();
         return blob
