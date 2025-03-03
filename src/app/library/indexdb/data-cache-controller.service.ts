dd63fb9a585528edb88b1270a7e494c706d21606
等等123你 <5358997+dengdeng123ni@user.noreply.gitee.com>
2025-03-03 19:40:06 +0800

    更新

--- src/app/library/indexdb/data-cache-controller.service.ts
+++ src/app/library/indexdb/data-cache-controller.service.ts
@@ -7,27 +7,27 @@ export class DataCacheControllerService {
 
   constructor() { }
 
-
   // 缓存数据
 
   // id 为缓存的唯一标识  data 为缓存的数据
   set_cache_data(id: string, data: JSON | Blob | ArrayBuffer | string | number | boolean) {
 
   }
-
+  // 获取缓存数据
   get_cache_data(id: string) {
 
   }
 
-
-  get_cache_date(id: string) {
+  // 获取缓存时间
+  get_cache_time(id: string) {
 
   }
 
+  // 删除缓存数据
   del_cache_data(id: string) {
 
   }
-
+  // json 转 md5
   JSON_to_md5(JSON: JSON): string {
     return '';
   }
